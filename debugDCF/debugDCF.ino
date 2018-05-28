b973a321e532c01dba5ccdd4b607bb8b903bc4e8
Memnod <christian@Christians-MacBook-Pro.local>
2018-05-28 16:08:24 +0200

    Added file for debug code

---
+++ debugDCF/debugDCF.ino
@@ -0,0 +1,11 @@
+/*This function can be used to analyze the signals coming from a DCF77 receiver*/
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
